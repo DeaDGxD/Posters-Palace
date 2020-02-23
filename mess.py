db9d3098f4d35cff577c5d385ed5a6a543723b91
DeaDGxD <59544724+DeaDGxD@users.noreply.github.com>
2020-02-23 16:42:29 -0600

    css overflow fix and some astetic changes

--- mess.py
+++ mess.py
@@ -1,13 +1,13 @@
-import sqlite3
-# car = datetime.datetime.now()
-# far = f"{car.day}-{car.month}-{car.year}  {car.hour}:{car.minute}"
-conn = sqlite3.connect('comments.db')
-c = conn.cursor()
-c.execute('''create table post(
-            name message_text ,
-            comment message_text,
-            time message_text 
-
-)''')
-conn.commit()
-conn.close()
+# import sqlite3
+# # car = datetime.datetime.now()
+# # far = f"{car.day}-{car.month}-{car.year}  {car.hour}:{car.minute}"
+# conn = sqlite3.connect('comments.db')
+# c = conn.cursor()
+# c.execute('''create table post(
+#             name message_text ,
+#             comment message_text,
+#             time message_text
+#
+# )''')
+# conn.commit()
+# conn.close()
